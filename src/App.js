1dfab7562db6ee0fa8dc22a76d3fe1feff65ef20
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2024-03-19 15:24:02 +0100

    api key added

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import ChatForm from "./Components/ChatForm";
+import Header from "./Components/Header";
+import AnswerSec from "./Components/Answer";
+import { Configuration, OpenAIApi } from "openai";
+import { useState } from "react";
+
+const App = () => {
+  const [messages, setMessages] = useState([]);
+
+  const configuration = new Configuration({
+    apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+  });
+
+  const openai = new OpenAIApi(configuration);
+
+  const responseGenerate = async (inputText, setInputText) => {
+    let options = {
+      prompt: `Complete this sentence: "${inputText}"`,
+      model: "text-davinci-003",
+      max_tokens: 50,
+      n: 1,
+      stop: ".",
+    };
+
+    let completeOptions = {
+      ...options,
+      prompt: inputText,
+    };
+
+    const response = await openai.createCompletion(completeOptions);
+
+    if (response.data.choices) {
+      setMessages([
+        {
+          question: inputText,
+          answer: response.data.choices[0].text,
+        },
+        ...messages,
+      ]);
+      setInputText("");
+    }
+  };
+  return (
+    <div className="App">
+      <Header />
+      <ChatForm responseGenerate={responseGenerate} />
+      <AnswerSec messages={messages} />
+    </div>
+  );
+};
+
+export default App;
